840dd48f8a912f4720c896b7b978b69e10e01be9
Adolfo_Tun <60202428+AdolfoMX@users.noreply.github.com>
2023-09-17 20:51:56 -0500

    Add files via upload

---
+++ pages_app/interfaz_patient/progress_record_patient.py
@@ -0,0 +1,27 @@
+import streamlit as st
+from streamlit_option_menu import option_menu
+
+def progress_record_patient_main():
+    with st.container():
+        
+        st.markdown(
+            """
+            <style>
+                .css-1y4p8pa {
+                    margin-top: -6rem;
+                }
+            </style>
+            """, unsafe_allow_html=True
+        )
+        
+        st.title("Registro de datos")
+        
+        selectd = option_menu(
+            menu_title=None,
+            options=["Hoja de evolución"],
+            orientation="horizontal"
+        )
+        
+        # Cuestionario
+        if selectd == "Hoja de evolución":
+            st.write("En construcción...")
