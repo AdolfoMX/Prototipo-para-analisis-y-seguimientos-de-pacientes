e8794e8f11a1bc5f0a5d563f138b012e83d05b26
Adolfo_Tun <60202428+AdolfoMX@users.noreply.github.com>
2023-09-26 22:06:57 -0500

    Add files via upload

--- database/code query.sql
+++ database/code query.sql
@@ -5,12 +5,12 @@ USE slsm_db;
 /*Creación de tablas*/
 CREATE TABLE usuarios (
 	id_usuario INT NOT NULL AUTO_INCREMENT,
-	nombre VARCHAR(25),
-	apellidos VARCHAR(25),
-    correo VARCHAR(25),
+	nombre VARCHAR(80),
+	apellidos VARCHAR(80),
+    correo VARCHAR(80),
     rol int,
-    contrasena VARCHAR(90),
-    contrasena_hash VARCHAR(100),
+    contrasena VARCHAR(100),
+    contrasena_hash VARCHAR(150),
     PRIMARY KEY(id_usuario)
 );
 
@@ -20,6 +20,25 @@ CREATE TABLE roles (
     PRIMARY KEY(id_rol)
 );
 
+/*
+TABLAS PENDIENTES POR CREAR..
+
+CREATE TABLE historiales_medicos (
+	id_historial INT NOT NULL AUTO_INCREMENT,
+    ...
+);
+
+CREATE TABLE hojas_evolucion_medico (
+	id_hojas INT NOT NULL AUTO_INCREMENT,
+    ...
+);
+
+CREATE TABLE avances_usuarios (
+	id_avance INT NOT NULL AUTO_INCREMENT,
+    ...
+);
+*/
+
 /*Agregar datos*/
 INSERT INTO roles (nombre_rol)
 VALUES ('Especialista'), ('Paciente');
@@ -27,4 +46,14 @@ VALUES ('Especialista'), ('Paciente');
 /*Consulta*/
 SELECT * FROM usuarios;
 SELECT * FROM roles;
-SELECT * FROM usuarios, roles
+
+SELECT 
+	usuarios.id_usuario, 
+    usuarios.nombre, 
+    usuarios.apellidos,
+    roles.nombre_rol 
+FROM 
+	usuarios, 
+    roles
+WHERE
+	usuarios.rol = roles.id_rol;
